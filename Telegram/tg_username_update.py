8630b25411a2f781ee1689dbe6ff17ddcb2fa465
2333 <cva.engineer.ding@gmail.com>
2019-01-07 16:19:56 +0800

    Create tg_username_update.py

---
+++ Telegram/tg_username_update.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Script updates last name of Telegram user every 10 seconds
+#
+# By @CodyDoby 
+# Telegram Group: https://t.me/google_dive
+# Blog: https://gfan.loan/
+#
+
+from telethon import TelegramClient, sync
+from telethon.tl.functions.account import UpdateProfileRequest
+from telethon.errors import SessionPasswordNeededError
+import time, sys
+import os.path
+from time import gmtime, strftime
+from random import random
+
+api_auth_file = 'api_auth'
+if not os.path.exists(api_auth_file+'.session'):
+    api_id = input('api_id: ')
+    api_hash = input('api_hash: ')
+else:
+    api_id = 123456
+    api_hash = '00000000000000000000000000000000'
+
+client = TelegramClient(api_auth_file, api_id, api_hash)
+client.start()
+
+#print('My infomation')
+#me = client.get_me()
+#print(me.stringify())
+print('Works!')
+
+# change localtimeï¼ln -sf /usr/share/zoneinfo/Asia/Chongqing /etc/localtime
+# https://stackoverflow.com/questions/4788533/python-strftime-gmtime-not-respecting-timezone
+while True:
+    try:
+        #time_cur = strftime("%H:%M:%S:%p:%a", time.localtime())
+        time_cur = strftime("%y:%m:%d:%H:%M:%S:%p:%a", time.localtime())
+        y, m, d, hour, minu, seco, p, abbwn = time_cur.split(':')
+
+        # Update every 10 seconds 
+        if int(seco) % 10 == 0:
+            for_fun = random()
+            # with a prob
+            if for_fun < 0.33:
+                last_name = '%s:%s' % (hour, minu)
+            elif for_fun < 0.66:
+                last_name = '%s:%s %s %s' % (hour, minu, p, abbwn)
+            else:
+                #last_name = '%s%s%s %s:%s' % (y, m, d, hour, minu)
+                last_name = ''
+                
+            client(UpdateProfileRequest(last_name=last_name))
+            print('Updated -> ', last_name)
+        # CPU sleep 
+        time.sleep(1)
+
+    except KeyboardInterrupt:
+        print('\nUsername resetting...\n')
+        client(UpdateProfileRequest(last_name=''))
+        sys.exit()
