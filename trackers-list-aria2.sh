95e776d6e00aa736c4ed900504615727922f22da
2333 <cva.engineer.ding@gmail.com>
2019-02-25 19:42:20 +0800

    Update trackers-list-aria2.sh

--- trackers-list-aria2.sh
+++ trackers-list-aria2.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #适用本仓库的aria2脚本安装
+PATH=/root/.aria2/aria2.conf
 service aria2 stop
 list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
-if [ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]; then
-    sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
+if [ -z "`grep "bt-tracker" ${PATH}`" ]; then
+    sed -i '$a bt-tracker='${list} ${PATH}
     echo add......
 else
-    sed -i "s@bt-tracker.*@bt-tracker=$list@g" /root/.aria2/aria2.conf
+    sed -i "s@bt-tracker.*@bt-tracker=$list@g" ${PATH}
     echo update......
 fi
 service aria2 start
