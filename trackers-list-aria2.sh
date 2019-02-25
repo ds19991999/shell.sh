c930bb29a429181f9dbb63dc6dd6bfec190a90a0
2333 <cva.engineer.ding@gmail.com>
2019-02-25 19:28:34 +0800

    Update trackers-list-aria2.sh

--- trackers-list-aria2.sh
+++ trackers-list-aria2.sh
@@ -6,7 +6,7 @@ if [ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]; then
     sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
     echo add......
 else
-    sed -i "s@bt-tracker.*@bt-tracker=$list@g" /root/.aria2/conf/aria2.conf
+    sed -i "s@bt-tracker.*@bt-tracker=$list@g" /root/.aria2/aria2.conf
     echo update......
 fi
 service aria2 start
